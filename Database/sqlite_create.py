384d3f6a7312c2d7fc93e9a103ab9ef8e1591b2f
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-06 01:49:01 -0500

    Feature: completed section on creating Session

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
-session = Session(bind=engine)
+from sqlalchemy.orm import sessionmaker, Session
 
+
+engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+Session = sessionmaker(bind=engine)
+
+session = Session()
