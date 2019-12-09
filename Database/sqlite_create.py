2543b2171dbca3ea47801004ea84365707afea10
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-08 20:19:13 -0500

    Feature: Added code to create sqlite database and tables

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -1,8 +1,57 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, SmallInteger
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
 
 
+# Create Engine that stores data in /web/Sqlite-Data/example.db file
 engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
-Session = sessionmaker(bind=engine)
 
-session = Session()
+# defining the models
+Base = declarative_base()
+
+
+# Creating tables
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
+
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    # orders = relationship("Order", backref='customer')
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now)
+    line_items = relationship("OrderLine", secondary="order_lines", backref='orders')
+
+
+class OrderLine(Base):
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(SmallInteger())
+    item = relationship("Item")
+
+
+# Create all tables in the database
+Base.metadata.create_all(engine)
