04a2868149f0c3909364ee2448bf963027127f0a
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-18 17:27:20 -0500

    Feature: Completed section on Inserting Data

--- Database/sqlite_insert.py
+++ Database/sqlite_insert.py
@@ -1,15 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+from sqlite_declarative import Base, Customer, Item, Order, OrderLine
 
 
 # Create Engine that stores data in web/Sqlite-Data/example.db file
-engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+# engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+engine = create_engine('sqlite:///Sqlite-Data/example.db')
+Base.metadata.bind = engine
+
 
 # Connect to sqlite server using absolute path
 # https://overiq.com/sqlalchemy-101/installing-sqlalchemy-and-connecting-to-database/
-Session = sessionmaker(bind=engine)
+DBSession = sessionmaker(bind=engine)
 
-session = Session()
+session = DBSession()
 
 # Inserting Data
 c1 = Customer(first_name='Toby',
@@ -26,15 +30,8 @@ c2 = Customer(first_name='Scott',
               email='scottharvey@example.com',
               address='424 Patterson Street',
               town='Beckinsdale'
-              )
-c1, c2
+)
 
-c1.first_name, c1.last_name
-c2.first_name, c2.last_name
 
-session.add(c1)
-session.add(c2)
-
-c1.id, c2.id
 session.add_all([c1, c2])
-session.new
+session.commit()
